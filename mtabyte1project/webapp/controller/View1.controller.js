585089b09b186f36d1f45b0202dcc2451bef8dcc
phani16y <phani.kumar.yennamani@sap.com>
2022-02-14 20:37:13 +0000

    changes for JWT-test

---
+++ mtabyte1project/webapp/controller/View1.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("mtabyte1project.controller.View1", {
+            onInit: function () {
+
+            }
+        });
+    });
---
+++ mtabyte1project/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"mtabyte1project/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
