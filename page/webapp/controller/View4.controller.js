6fc032d8efa75ca2153692d894b84d111f74f306
Charan Samanchi <charan.samanchi@sap.com>
2022-02-14 12:10:07 +0000

    Commit changes for field glass popup

--- page/webapp/controller/View4.controller.js
+++ page/webapp/controller/View4.controller.js
@@ -1,17 +1,18 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/m/MessageBox"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,JSONModel, MessageToast) {
+    function (Controller,JSONModel, MessageToast, MessageBox) {
         "use strict";
 
         return Controller.extend("page.controller.View4", {
             onInit: function () {
-                var userDetailsModel = new sap.ui.model.json.JSONModel();
+                var userDetailsModel = new JSONModel();
                 // this.userDetailsModel = new JSONModel({});
                 this.getView().setModel(this.userDetailsModel, "UserDetailsModel");
                 var authInfoData = {};
@@ -39,10 +40,35 @@ sap.ui.define([
                 window.open('https://supplier.ariba.com', '_blank');
                 MessageToast.show("Opened in new tab.");
             },
-            press1 : function(evt) {
-                debugger;
-                window.open('https://xcore2.fgvms.com/SSOLogin?SSOParams=company%3DNINA%3B%3Breturningurl%3DcloseWindow', '_blank');
-                MessageToast.show("Opened in new tab.");
+            press1 : function(oEvent) {
+            debugger;
+            var oTitle = oEvent.getSource().getProperty("header");
+		//	var oTargetSource = oEvent.getParameter("itemPressed").getTargetSrc();
+				MessageBox.confirm(
+					"Agree to Terms and Conditions below.", {
+						icon: MessageBox.Icon.INFORMATION,
+						title: "Terms and Conditions",
+						details: "<p><strong>By clicking Agree and Continue</strong></p>\n" +
+						"<ul>" +
+						"<li>I hereby give my consent to the User Agreement, it´s policies.</li>" +
+						"<li>I instruct SAP Fieldglass to use my Ariba Network Credentials to login.</li>" +
+						"<li>In addition I also agree on the Terms and Conditions from El Corte Inglés.</li>" +
+						"</ul>" +
+						"<p><a href='https://www.elcorteingles.com/luxe/for-shopping-lovers-rewards/terms-y-conditions/' target='_blank'>Click here</a> for Terms and Conditions Document",
+						contentWidth: "700px",
+						actions: ["Agree and Continue", "Cancel"],
+						emphasizedAction: "Agree and Continue",
+						onClose: function (oAction) { 
+							if (oAction === "Agree and Continue"){
+								sap.m.URLHelper.redirect('https://xcore2.fgvms.com/SSOLogin?SSOParams=company%3DNINA%3B%3Breturningurl%3DcloseWindow', true);
+                          //      window.open(', '_blank');
+                                MessageToast.show("Opened in new tab.");
+							}
+						}
+					}
+				);
+
+			this.fnClose();
             },
         });
     });
